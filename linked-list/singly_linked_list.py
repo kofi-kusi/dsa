dde292d79dffe9f498035d15dbfbbb86ef4cb5ad
Kofi Kusi Appau <appaukusi05@gmail.com>
2025-05-28 18:48:55 +0000

    add sll

---
+++ linked-list/singly_linked_list.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # add node at the last position
+    def insert_end(self, val):
+        new_node = Node(val)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+
+        curr.next = new_node
+
+    def insert_at_position(self, val, pos):
+        temp = 1
+        new_node = Node(val)
+        curr = self.head
+
+        # move to the node just before the required position
+        while temp < pos:
+            curr = curr.next
+            temp += 1
+
+        new_node.next = curr.next
+        curr.next = new_node
+
+
+    # removing a node at an index
+    def remove(self, index):
+        i = 0
+        curr = self.head
+        while i < index and curr:
+            curr = curr.next
+            i += 1
+
+        if curr:
+            curr.next = curr.next.next
+
+    def print(self):
+        curr = self.head
+        while curr:
+            print(curr.val, "->")
