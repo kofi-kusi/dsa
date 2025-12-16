0f72139264fd6388aece10e1f28d9e9cbb1e1a26
Kofi Kusi Appau <appau.kofi.kusi@gmail.com>
2025-12-16 08:49:18 +0000

    Add example 2

--- searching-and-sorting-algo/selectionSort.py
+++ searching-and-sorting-algo/selectionSort.py
@@ -1,3 +1,4 @@
+# Example 1
 def selectionSort(L):
     for i in range(len(L)):
         minItem = i
@@ -8,5 +9,34 @@ def selectionSort(L):
 
     return L
 
-L = [1, 5, 3, 4, 2, 0]
-print(selectionSort(L))
+# L = [1, 5, 3, 4, 2, 0]
+# print(selectionSort(L))
+
+
+# Example 2: sorting an array of numbers by selecting and poping the smallest
+#            into a new array
+
+# Helper function to find the smallest number
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+
+    return smallest_index
+
+
+# sorting algo
+def selectionSort2(arr):
+    newArr = []
+    copiedArr = arr[:]
+
+    for i in range(len(copiedArr)):
+        smallest = findSmallest(copiedArr)
+        newArr.append(copiedArr.pop(smallest))
+    return newArr
+
+# print(selectionSort2([5, 3, 6, 2, 10]))
